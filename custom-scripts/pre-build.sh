7bf1ddd5b7e77f3669d5e7aa9500485089887598
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-09-07 00:10:32 +0000

    tutorial 3, feito

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,3 +2,6 @@
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+
+cp $BASE_DIR/../custom-scripts/hello/S50hello $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/S50hello
