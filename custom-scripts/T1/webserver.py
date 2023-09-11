f61320a43e669095321dd4798a2f171896013bcc
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-09-11 23:45:34 +0000

    startando o servidor durante a inicialização da target

--- custom-scripts/T1/webserver.py
+++ custom-scripts/T1/webserver.py
@@ -1,9 +1,7 @@
-import time
 from http.server import BaseHTTPRequestHandler,HTTPServer
 import os
 
-
-HOST_NAME = '0.0.0.0' # !!!REMEMBER TO CHANGE THIS!!!
+HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 8000
 
 
@@ -16,26 +14,22 @@ class MyHandler(BaseHTTPRequestHandler):
         """Respond to a GET request."""
 
         datahora = os.popen('date').read()
+        uptime = os.popen('cat /proc/uptime').read()
+        cpu_name = os.popen('grep "model name" /proc/cpuinfo').read()
+        cpu_vel = os.popen('grep "cpu MHz" /proc/cpuinfo').read()
 
 
         s.send_response(200)
         s.send_header("Content-type", "text/html")
         s.end_headers()
-        s.wfile.write("<html><head><title>Title goes here.</title></head>".encode())
-        s.wfile.write("<body><p>This is a test.</p>".encode())
         s.wfile.write(f"<p>Data e Hora: {datahora}</p>".encode())
+        s.wfile.write(f"<p>Uptime: {uptime}</p>".encode())
+        s.wfile.write(f"<p>Modelo do processador e velocidade: {cpu_name} {cpu_vel} MHz</p>".encode())
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
-        s.wfile.write(f"<p>You accessed path: {s.path}</p>".encode())
         s.wfile.write("</body></html>".encode())
 
 if __name__ == '__main__':
     httpd = HTTPServer((HOST_NAME, PORT_NUMBER), MyHandler)
-    print("Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print("Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
-
+    # print("Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
+    httpd.serve_forever()
--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -3,8 +3,8 @@
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 
-cp $BASE_DIR/../custom-scripts/hello/S50hello $BASE_DIR/target/etc/init.d
-chmod +x $BASE_DIR/target/etc/init.d/S50hello
-
 cp $BASE_DIR/../custom-scripts/T1/webserver.py $BASE_DIR/target/usr/bin
 chmod +x $BASE_DIR/target/usr/bin/webserver.py
+
+cp $BASE_DIR/../custom-scripts/T1/Swebserver $BASE_DIR/target/etc/init.d
+chmod +x $BASE_DIR/target/etc/init.d/Swebserver
