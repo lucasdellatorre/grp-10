fccfbc67e8881492766be08322504c1d97d0458e
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-09-13 21:18:22 +0000

    algumas coisas

--- custom-scripts/T1/webserver.py
+++ custom-scripts/T1/webserver.py
@@ -17,10 +17,10 @@ class MyHandler(BaseHTTPRequestHandler):
         uptime = os.popen('cat /proc/uptime').read()
         cpu_name = os.popen('grep "model name" /proc/cpuinfo').read()
         cpu_vel = os.popen('grep "cpu MHz" /proc/cpuinfo').read()
-        #Capacidade ocupada do processador
+        proc_capacity = os.popen('awk "/^cpu / {idle=$5; total=0; for (i=2; i<=NF; i++) total+=$i} END {print 100 - (idle / total * 100)}" /proc/stat')
         ram = os.popen('grep -E "MemTotal|MemAvailable" /proc/meminfo').read()
         sys_version = os.popen('cat /proc/version').read()
-        #Lista de processos em execução
+        proc_list = os.popen("ps aux | awk '{print $2, $11}'").read()
 
 
         s.send_response(200)
@@ -29,8 +29,10 @@ class MyHandler(BaseHTTPRequestHandler):
         s.wfile.write(f"<p>Data e Hora: {datahora}</p>".encode())
         s.wfile.write(f"<p>Uptime: {uptime}</p>".encode())
         s.wfile.write(f"<p>Modelo do processador e velocidade: {cpu_name} {cpu_vel} MHz</p>".encode())
+        s.wfile.write(f"<p>Capacidade ocupada do processador: {proc_capacity}</p>".encode())
         s.wfile.write(f"<p>Quantidade de memória RAM total e usada : {ram}</p>".encode())
-        s.wfile.write(f"<p>System version: {sys_version}</p>".encode())
+        s.wfile.write(f"<p>Versao do sistema: {sys_version}</p>".encode())
+        s.wfile.write(f"<p>Lista de processos em execução (pid e nome): {proc_list}</p>".encode())
         # If someone went to "http://something.somewhere.net/foo/bar/",
         # then s.path equals "/foo/bar/".
         s.wfile.write("</body></html>".encode())
