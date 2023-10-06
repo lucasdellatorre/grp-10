717f035d2fe9d9a5b0c71bf9e82c2f051eff6e90
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-10-06 22:01:51 +0000

    2.4

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -8,3 +8,8 @@ chmod +x $BASE_DIR/target/usr/bin/webserver.py
 
 cp $BASE_DIR/../custom-scripts/T1/Swebserver $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/Swebserver
+
+make -C $BASE_DIR/../modules/simple_driver/
+
+cp $BASE_DIR/../disk-test/hello $BASE_DIR/target/usr/bin
+chmod +x $BASE_DIR/target/usr/bin/hello
