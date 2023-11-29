8386b8b0f8a1ca7bcb0561df0c1d658215793267
Gabriel-Tabajara <gataba.santos@gmail.com>
2023-11-29 20:56:12 +0000

    quemu comand

--- custom-scripts/T3/sched_profiler.c
+++ custom-scripts/T3/sched_profiler.c
@@ -135,3 +135,4 @@ void print_scheduled_threads_summary(int *arr)
 
 //sudo unshare --pid --fork bash
 
+// qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --hdb sdb.bin --nographic --append "console=ttyS0 root=/dev/sda"
